96cb75014a848ff51dd79f31c63c161776bdef11
Jack Isherwood <jisherwood12@gmail.com>
2017-11-18 15:08:58 -0800

    Added support for usage api

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const utils = require("./utils.js")
 
 function getFinance(options, cb) {
   // required fields in the options object
-  let required = ['category', 'entity', 'period', 'apiKey'];
+  let required = ['category', 'entity', 'period', 'apikey'];
   // checkOptions verifies that the required fields are present.
   utils.checkOptions(required, options);
-  return utils.getDatavideApi(`finance/${options.category}/${options.entity}/${options.period}`, options.apiKey, cb);
+  return utils.getDatavideApi(`finance/${options.category}/${options.entity}/${options.period}`, options.apikey, cb);
 }
 
 function getEconomics(options, cb) {
-  let required = ['category', 'country', 'indicatorName', 'apiKey'];
+  let required = ['category', 'country', 'indicatorName', 'apikey'];
   utils.checkOptions(required, options);
-  return utils.getDatavideApi(`economics/${options.category}/${options.country}/${options.indicatorName}`, options.apiKey, cb)
+  return utils.getDatavideApi(`economics/${options.category}/${options.country}/${options.indicatorName}`, options.apikey, cb)
 }
 
 function getCompanies(options, cb) {
-  let required = ['category', 'entity', 'period', 'apiKey'];
+  let required = ['category', 'entity', 'period', 'apikey'];
   utils.checkOptions(required, options);
-  return utils.getDatavideApi(`finance/${options.category}/${options.entity}/${options.period}`, options.apiKey, cb)
+  return utils.getDatavideApi(`finance/${options.category}/${options.entity}/${options.period}`, options.apikey, cb)
 }
 
 function getSports(options, cb) {
@@ -42,10 +42,15 @@ function getSports(options, cb) {
     }
   }
 
-  return utils.getDatavideApi(request, options.apiKey, cb)
+  return utils.getDatavideApi(request, options.apikey, cb)
+}
+
+function getUsage(apikey, cb) {
+  return utils.getDatavideApi("usage", apikey, cb)
 }
 
 exports.getFinance   = getFinance
 exports.getEconomics = getEconomics
 exports.getCompanies = getCompanies
 exports.getSports    = getSports
+exports.getUsage     = getUsage
